d64af09eebd65485d1134a7a924d7abcfb51758d
Keenan Nemetz <keenan.nemetz@gmail.com>
2021-12-28 19:39:02 -0600

    cleanup tests

--- packages/valist-contracts/test/index.ts
+++ packages/valist-contracts/test/index.ts
@@ -3,11 +3,16 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Contract, Signer } from "ethers";
 
+async function deploy() {
+  const Valist = await ethers.getContractFactory("Valist");
+  const valist = await Valist.deploy();
+  await valist.deployed();
+  return valist;
+}
+
 describe("Valist", () => {
   it("Should create a team", async function() {
-    const Valist = await ethers.getContractFactory("Valist");
-    const valist = await Valist.deploy();
-    await valist.deployed();
+    const valist = await deploy();
 
     const accounts = await ethers.getSigners();
     const members = accounts.map(async (acct) => await acct.getAddress());
@@ -17,9 +22,7 @@ describe("Valist", () => {
   });
 
   it("Should fail to create a team with claimed name", async function() {
-    const Valist = await ethers.getContractFactory("Valist");
-    const valist = await Valist.deploy();
-    await valist.deployed();
+    const valist = await deploy();
 
     const accounts = await ethers.getSigners();
     const members = accounts.map(async (acct) => await acct.getAddress());
@@ -30,18 +33,14 @@ describe("Valist", () => {
   });
 
   it("Should fail to create a team with no members", async function() {
-    const Valist = await ethers.getContractFactory("Valist");
-    const valist = await Valist.deploy();
-    await valist.deployed();
+    const valist = await deploy();
 
     await expect(valist.createTeam("acme", "Qm", []))
       .to.be.revertedWith('err-empty-members');
   });
 
   it("Should fail to create a team with no name", async function() {
-    const Valist = await ethers.getContractFactory("Valist");
-    const valist = await Valist.deploy();
-    await valist.deployed();
+    const valist = await deploy();
 
     const accounts = await ethers.getSigners();
     const members = accounts.map(async (acct) => await acct.getAddress());
@@ -51,9 +50,7 @@ describe("Valist", () => {
   });
 
   it("Should fail to create a team with no meta", async function() {
-    const Valist = await ethers.getContractFactory("Valist");
-    const valist = await Valist.deploy();
-    await valist.deployed();
+    const valist = await deploy();
 
     const accounts = await ethers.getSigners();
     const members = accounts.map(async (acct) => await acct.getAddress());
