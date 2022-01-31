fa9e146e5d3fc696b7c5053521447b7a44644037
jiyuu-jin <zach@valist.io>
2022-01-31 13:49:13 -0500

    Add valist-sdk

--- packages/valist-web/next.config.js
+++ packages/valist-web/next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     MAGIC_PUBKEY: 'pk_live_B577A4A7B11805D0',
     METATX_ENABLED: process.env.METATX_ENABLED || true,
   },
+  webpack: function (config, options) {
+    if (!options.isServer) {
+      // polyfill events on browser. since webpack5, polyfills are not automatically included
+      config.resolve.fallback.events = require.resolve('events/')
+    }
+    config.plugins.push(new options.webpack.IgnorePlugin({ resourceRegExp: /^electron$/ }));
+    return config;
+  },
 };
 
 module.exports = nextConfig;
