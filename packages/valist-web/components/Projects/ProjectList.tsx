c29e608beccb36fe3aa510b724f22e9ac8306c3b
jiyuu-jin <zach@valist.io>
2022-02-02 04:17:23 -0500

    Fix Project list doesn’t clear when switching to a new account with an empty project list

--- packages/valist-web/components/Projects/ProjectList.tsx
+++ packages/valist-web/components/Projects/ProjectList.tsx
@@ -1,44 +1,44 @@
-import { Fragment, useContext, useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
-import ProjectCard from "./ProjectCard";
+import ProjectListCard from "./ProjectListCard";
 import Link from "next/link";
 import { USER_PROJECTS } from "../../utils/Apollo/queries";
-import AccountContext from "../Accounts/AccountContext";
 import { Project } from '../../utils/Apollo/types';
 
-export default function ProjectList() {
-  const accountCtx = useContext(AccountContext);
-  const [userProjects, setUserProjects] = useState<Project[]>([{
-    id: 'loading',
-    name: 'loading',
-    metaURI: 'loading',
-    team: {name: 'loading'}
-  }]);
+interface ProjectListProps {
+  address: string,
+}
+
+export default function ProjectList(props: ProjectListProps) {
+  const [userProjects, setUserProjects] = useState<Project[]>([]);
   const { data, loading, error } = useQuery(USER_PROJECTS, {
-    variables: { address: accountCtx.address.toLowerCase() },
+    variables: { address: props.address.toLowerCase() },
   });
 
   useEffect(() => {
     if (data && data.users && data.users[0]) {
       setUserProjects(data.users[0].projects);
+    } else if (data) {
+      setUserProjects([]);
     }
-  }, [data, loading, error]);
+  }, [data, loading, error, setUserProjects, props.address]);
 
   return (
-  <div>
-    <div className="grid grid-cols-2 gap-2">
-      {userProjects && userProjects.map((project: Project) => (
-        <Fragment key={project.id}>
-          <Link href={`/${project.team.name}/${project.name}`}>
-            <a>
-              <ProjectCard 
-                teamName={project.team.name} 
-                projectName={project.name}
-                metaCID={project.metaURI} />
-            </a>
-          </Link>
-        </Fragment>
-      ))}
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+        {userProjects && userProjects.map((project: Project) => (
+          <Fragment key={project.id}>
+            <Link href={`/${project.team.name}/${project.name}`}>
+              <a>
+                <ProjectListCard 
+                  teamName={project.team.name} 
+                  projectName={project.name}
+                  metaURI={project.metaURI} />
+              </a>
+            </Link>
+          </Fragment>
+        ))}
+      </div>
     </div>
-  </div>)
+  )
 }
---
+++ packages/valist-web/components/Projects/ProjectListCard.tsx
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { fetchJSONfromIPFS, parseCID } from '../../utils/Ipfs';
+import { ProjectMeta } from '../../utils/Valist/types';
+import AddressIdenticon from '../Identicons/AddressIdenticon';
+import ValistContext from '../Valist/ValistContext';
+
+type ProjectCardProps = {
+  teamName: string,
+  projectName: string,
+  metaURI: string,
+};
+
+export default function ProjectListCard({ teamName, projectName, metaURI }: ProjectCardProps): JSX.Element {
+  const name = `${teamName}/${projectName}`;
+  const valistCtx = useContext(ValistContext);
+  let [ meta, setMeta ] = useState<ProjectMeta>({
+    image: '',
+    description: "Loading....",
+  });
+
+  useEffect(() => {
+    if (metaURI === 'loading') {
+      setMeta({description: metaURI});
+    } else {
+      fetchJSONfromIPFS(metaURI).then((metaJson) => {
+        setMeta(JSON.parse(metaJson));
+      });
+    }
+  }, [metaURI]);
+
+  return (
+    <div className="bg-white rounded-lg shadow px-8 py-6 mb-2 flex items-center justify-start border-2 hover:border-indigo-500">
+      <div className="mr-7">
+         {meta && meta.image && meta.image !== '' && 
+          <img height={80} width={80} className='mx-auto rounded-full'
+           src={`${valistCtx.ipfsGateway}/ipfs/${parseCID(meta.image)}`} alt="" />           
+         }
+         {meta && meta.image && meta.image === '' && 
+          <AddressIdenticon address={name} height={80} />
+         }
+      </div>
+      <div className="">
+        <h3 className="text-xl">
+          {projectName}
+        </h3>
+        <div>Published by: 
+          <span className="ml-1 cursor-pointer text-gray-900 py-1">
+            <span style={{marginBottom: "-4px"}} className='inline-block'></span>
+              {teamName}
+            </span>
+        </div>
+        <p>
+          {meta.description}
+        </p>
+      </div>
+    </div>
+  )
+};
