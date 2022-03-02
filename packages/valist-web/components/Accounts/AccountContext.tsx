aa2df0297f9c89432ceed810ebe99a9f9c8b9309
jiyuu-jin <zach@valist.io>
2022-03-02 04:20:08 -0500

    Add project donation form.

--- packages/valist-web/components/Accounts/AccountContext.tsx
+++ packages/valist-web/components/Accounts/AccountContext.tsx
@@ -5,10 +5,12 @@ export default React.createContext<AccountCtxInterface>({
   magic: null,
   address: '0x0',
   loginType: 'readOnly',
+  modal: false,
   setShowLogin: () => {},
   setLoginType: () => {},
   setAddress: () => {},
   setMagic: () => {},
   notify: (type: string, text?: string) => { return type + text; },
   dismiss: (type: string) => { console.log(type); },
+  setModal: () => {},
 });
