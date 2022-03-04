3d8b75761d0a155b35fe14fe86063de6b9a3e37e
jiyuu-jin <zach@valist.io>
2022-03-03 21:23:48 -0500

    Wording changes to create team form

--- packages/valist-web/components/Publishing/CreateTeamForm.tsx
+++ packages/valist-web/components/Publishing/CreateTeamForm.tsx
@@ -127,7 +127,7 @@ export default function CreateTeamForm(props: CreateTeamFormProps) {
             rows={4}
             className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block 
             w-full sm:text-sm border border-gray-300 rounded-md"
-            placeholder={'An example team description'}
+            placeholder={'An example description'}
             defaultValue={''}
           />
         </div>
