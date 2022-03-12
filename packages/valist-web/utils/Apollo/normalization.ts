c03fed7fd3ffd30b244fe71d722a56b3942d5f5b
jiyuu-jin <zach@valist.io>
2022-03-11 19:09:30 -0500

    Added acount dropdown to homepage and account fixes to create form.
    Co-authored-by: Alec M. Wantoch <awantoch@users.noreply.github.com>

---
+++ packages/valist-web/utils/Apollo/normalization.ts
@@ -0,0 +1,41 @@
+import { Team } from "../Valist/types";
+import { Project } from "./types";
+
+export function normalizeUserProjects(teamList: Team[], projectList:Project[]) {
+  const rawTeams: Team[] = teamList;
+  const teamNames = [];
+  let teams: Record<string, Project[]> = {};
+
+  for (let i = 0; i < rawTeams.length; i++) {
+    teams[rawTeams[i].name] = rawTeams[i].projects;
+    teamNames.push(rawTeams[i].name);
+  }
+
+  if (projectList) {
+    const rawProjects: Project[] = projectList;
+    for (let i = 0; i < rawProjects.length; i++) {
+      let projectTeamName = rawProjects[i].team.name;
+      if (!Object.keys(teams).includes(projectTeamName)) {
+        if (teams[projectTeamName]?.includes(rawProjects[i])) {
+          teams[projectTeamName].push(rawProjects[i]);
+        } else {
+          teams[projectTeamName] = [rawProjects[i]];
+          teamNames.push(rawProjects[i].team.name);
+        }
+      }
+    }
+  }
+
+  return { teamNames, teams };
+}
+
+export function getProjectNames(teams:Record<string, Project[]>, teamName: string) {
+  const firstTeamProjects = teams[teamName];
+  const projectNames:string[] = [];
+
+  for (let i = 0; i < firstTeamProjects?.length; i++) {
+    projectNames.push(firstTeamProjects[i].name);
+  }
+
+  return projectNames;
+}
