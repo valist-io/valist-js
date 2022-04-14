b7dbbc5dbfd9edf5900a07ffa8d0e087c67f7dcc
Alec M. Wantoch <alec@wantoch.com>
2022-04-14 12:27:08 -0400

    remove assetprefix

--- packages/valist-web/next.config.js
+++ packages/valist-web/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './',
+  // assetPrefix: './',
   images: {
-    domains: ['valist.mypinata.cloud', 'gateway.valist.io', 'https://gateway.valist.io', 'localhost'],
+    domains: ['gateway.valist.io', 'https://gateway.valist.io', 'localhost'],
   },
   publicRuntimeConfig: {
     CHAIN_ID: process.env.CHAIN_ID || 1337,
