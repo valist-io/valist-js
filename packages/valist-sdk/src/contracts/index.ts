bf03835d960663c7b7df9b3639dd0eefcfb41b16
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-04-14 14:59:10 -0700

    update addresses

--- packages/valist-sdk/src/contracts/index.ts
+++ packages/valist-sdk/src/contracts/index.ts
@@ -12,7 +12,7 @@ export function getRegistryAddress(chainId: number): string {
 		case 137: // Polygon mainnet
 			return '0xc70A069eC7F887a7497a4bdC7bE666C1e18c8DC3';
 		case 80001: // Mumbai testnet
-			return '0xC2E442A911A70097093E5d51A9224A1587D888cb';
+			return '0x83B5f729DB18E2Cae6388A414210543581f9df17';
 		case 1337: // Deterministic Ganache
 			return '0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab';
 		default:
@@ -25,7 +25,7 @@ export function getPaymasterAddress(chainId: number): string {
 		case 137: // Polygon mainnet
 			return 'TODO';
 		case 80001: // Mumbai testnet
-			return '0xf58cd651b4CDF7E32F5C148139eaacbc72272872';
+			return '0xe59447Fb03f44Af0a483840a7A6059a4B03FB4fC';
 		case 1337: // Deterministic Ganache
 			return '0xCfEB869F69431e42cdB54A4F4f105C19C080A601';
 		default:
@@ -38,7 +38,7 @@ export function getLicenseAddress(chainId: number): string {
 		case 137: // Polygon mainnet
 			return '0xb85ed41d49Eba25aE6186921Ea63b6055903e810';
 		case 80001: // Mumbai testnet
-			return '0x980Fb2419437e5E4B8dB2AdCD6Ff374b9Ef1b688';
+			return '0xF0D58AacF271629B2c598d557E8c2FCF047FfD30';
 		case 1337: // Deterministic Ganache
 			return '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24';
 		default:
