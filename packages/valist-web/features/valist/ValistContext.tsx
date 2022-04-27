096af5ae41b819bf2934e3730db2bd43aa83f228
jiyuu-jin <zach@valist.io>
2022-04-27 13:50:57 -0400

    Fix type error valistCt is possibly null

--- packages/valist-web/features/valist/ValistContext.tsx
+++ packages/valist-web/features/valist/ValistContext.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
-import { Client } from '@valist/sdk';
+import { Client, createReadOnly } from '@valist/sdk';
+import { defaultProvider } from '../../utils/Providers';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
-export default React.createContext<Client | null | undefined>(null);
+export default React.createContext<Client>(createReadOnly(defaultProvider,
+  {
+    chainId: Number(publicRuntimeConfig.CHAIN_ID),
+  },
+));
