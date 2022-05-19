087ffdf772549c301ba553a41bd3ec251e0b0f1f
jiyuu-jin <zach@valist.io>
2022-05-19 12:16:25 -0400

    Ad some basic jest tests

---
+++ packages/valist-web/__tests__/tabs.test.tsx
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Tabs from "../components/Tabs";
+
+describe("Tabs", () => {
+  const props = {
+    view: 'Tab1',
+    setView: () => {},
+    tabs: [
+      {
+        text: 'Tab1',
+        disabled: false,
+      },
+      { 
+        text: 'Tab2',
+        disabled: true,
+      },
+    ],
+  };
+  
+  it("Tabs are rendered in list", () => {
+    render(<Tabs {...props}/>);
+
+    const tab1Element = screen.getByText('Tab1');
+    const tab2Element = screen.getByText('Tab2');
+    expect(tab1Element && tab2Element);
+  });
+
+  it("Active tab has class text-indigo-500", () => {
+    render(<Tabs {...props}/>);
+
+    const tab1Element = screen.getByText('Tab1');
+    expect(tab1Element.className.includes('text-indigo-500'));
+  });
+
+  it("Enabled tab has class cursor-pointer", () => {
+    render(<Tabs {...props}/>);
+
+    const tab1Element = screen.getByText('Tab1');
+    expect(tab1Element.className.includes('cursor-pointer'));
+  });
+
+  it("Disabled tab has class cursor-not-allowed", () => {
+    render(<Tabs {...props}/>);
+
+    const tab2Element = screen.getByText('Tab2');
+    expect(tab2Element.className.includes('cursor-not-allowed'));
+  });
+});
