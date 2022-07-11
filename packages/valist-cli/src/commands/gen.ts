662872df9cd721e851aedfb073ba9d101c29c62c
jiyuu-jin <zach@valist.io>
2022-07-11 15:10:22 -0400

    Add valist gen command

---
+++ packages/valist-cli/src/commands/gen.ts
@@ -0,0 +1,23 @@
+import { Command } from '@oclif/core';
+import keytar from 'keytar';
+import { ethers } from 'ethers';
+import { randomBytes } from 'crypto';
+
+export default class Gen extends Command {
+  static description = 'Generate a new account'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+  ]
+
+  async createSignerKey(): Promise<ethers.Wallet> {
+    const key = randomBytes(32).toString('hex');
+    return new ethers.Wallet(key);
+  }
+
+  public async run(): Promise<void> {
+    const wallet = await this.createSignerKey();
+    await keytar.setPassword('VALIST', wallet.address, wallet.privateKey);
+    this.log(`successfully generated ${wallet.address}`);
+  }
+}
