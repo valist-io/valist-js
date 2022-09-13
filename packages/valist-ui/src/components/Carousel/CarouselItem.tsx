fbc120b3e1b0be9999b860929e80d7e8a5901919
jiyuu-jin <zach@valist.io>
2022-09-13 18:38:01 -0400

    Add stats utils

--- packages/valist-ui/src/components/Carousel/CarouselItem.tsx
+++ packages/valist-ui/src/components/Carousel/CarouselItem.tsx
@@ -14,17 +14,17 @@ export function CarouselItem(props: DiscoveryItemProps) {
   const descColor = theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.dark[4];
 
   return (
-    <div>
+    <div style={{maxWidth: 350}}>
       <Link href={props.link} passHref>
         <Card shadow="sm" p="lg" sx={() => ({
           '&:hover': {
-            border: '2px solid #5850EC',
+            cursor: 'pointer',
           },
         })}>
           <Image src={props.img} radius="md" height={160} alt={props.name} />
           <div>
             <Text style={{ margin: '16px 0 8px 0' }} weight={900}>{props.name}</Text>
-            <Text style={{ margin: 0, height: 44, color: descColor }} size="sm">
+            <Text style={{ margin: 0, height: 44, overflow: 'hidden', color: descColor }} size="sm">
               {props.description}
             </Text>
           </div>
