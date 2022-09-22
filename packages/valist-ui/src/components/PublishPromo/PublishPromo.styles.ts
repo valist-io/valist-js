80f87a280e8689e611bfba3133b68cbc2bf6d574
jiyuu-jin <zach@valist.io>
2022-09-22 19:13:53 -0400

    Fix publish promo on dark mode

--- packages/valist-ui/src/components/PublishPromo/PublishPromo.styles.ts
+++ packages/valist-ui/src/components/PublishPromo/PublishPromo.styles.ts
@@ -5,6 +5,7 @@ import { useMantineTheme } from "@mantine/core";
 export default createStyles(() => {
   const isMobile = useMediaQuery('(max-width: 900px)');
   const theme = useMantineTheme();
+  const bgImg = theme.colorScheme === 'dark' ? `url('/images/discovery/publish_bg.png')` : '',
   const bgColor = theme.colorScheme === 'dark' ? '' : '#5850EC';
   const btnColor = theme.colorScheme === 'dark' ? '#5850EC' : 'linear-gradient(90deg, #FF9A9E 0%, #FAD0C4 99%, #FAD0C4 100%), #5850EC';
   const paddingY = isMobile ? '24px' : '64px';
@@ -14,15 +15,18 @@ export default createStyles(() => {
       padding: `0 ${paddingY} 0 ${paddingY}`,
       background: bgColor,
       maxWidth: '100vw',
+      backgroundImage: bgImg,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
     },
     "heading": {
-      fontSize: isMobile ? 40 : 56, 
-      color: 'white', 
+      fontSize: isMobile ? 40 : 56,
+      color: 'white',
       fontWeight: 900,
       margin: 0,
     },
     "text": {
-      fontSize: 21, 
+      fontSize: 21,
       color: 'white',
       margin: '10px 0 25px 0',
     },
@@ -31,7 +35,7 @@ export default createStyles(() => {
     },
     "msgBox": {
       height: '100%',
-      display: 'flex', 
+      display: 'flex',
       alignItems: 'center',
     }
   };
