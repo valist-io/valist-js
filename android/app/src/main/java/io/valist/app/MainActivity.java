a056f69a141273a385f1e8acec12cdf1665b8fe7
jiyuu-jin <zach@valist.io>
2022-10-12 18:19:27 -0400

    Add android build & workflow

---
+++ android/app/src/main/java/io/valist/app/MainActivity.java
@@ -0,0 +1,15 @@
+package io.valist.app;
+
+import android.content.res.Configuration;
+import android.webkit.WebSettings;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+}
+
+
+
+
+
+
---
+++ android/app/src/main/java/io/valist/app/SplashScreen.java
@@ -0,0 +1,13 @@
+package io.valist.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class SplashScreen extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
