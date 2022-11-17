a0d88bc2247b5fc523d3a2e070fee35218fd05ec
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-11-17 12:35:59 -0800

    Revert "remove magic connector"
    This reverts commit fbfde7d7aa908578aac5fd66e81381bcb7781226.

--- packages/valist-web/components/RainbowKitProvider/connectors.ts
+++ packages/valist-web/components/RainbowKitProvider/connectors.ts
@@ -1,8 +1,34 @@
 import { connectorsForWallets, wallet, Wallet } from '@rainbow-me/rainbowkit';
+import { MagicConnector } from '@everipedia/wagmi-magic-connector';
 import { Chain } from 'wagmi';
+
+export const magic = ({ chains }: any): Wallet => ({
+  id: 'magic',
+  name: 'Email',
+  iconUrl: 'images/mail.svg',
+  iconBackground: '#1E88E5',
+  // @ts-ignore
+  createConnector: () => {
+    const connector = new MagicConnector({
+      chains: chains,
+      options: {
+        apiKey: 'pk_live_631BA2340BB9ACD8',
+        additionalMagicOptions: {
+          network: {
+            rpcUrl: 'https://polygon-rpc.com',
+            chainId: 137,
+          },
+        },
+      },
+    });
+
+    return { connector };
+  },
+});
 
 export const connectors = (chains: Chain[]) => {
   const wallets = [
+    magic({ chains }),
     wallet.rainbow({ chains }),
     wallet.walletConnect({ chains }),
   ];
