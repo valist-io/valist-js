535a93d342a32421452bfa2cc7421ca37b0cb20c
jiyuu-jin <zach@valist.io>
2022-12-02 13:33:56 -0500

    Fix pricing item styles on darkmode

--- packages/valist-ui/src/components/Item/Item.styles.ts
+++ packages/valist-ui/src/components/Item/Item.styles.ts
@@ -1,15 +1,16 @@
 import { createStyles } from "@mantine/core";
 
 export default createStyles((theme, _params, _getRef) => {
+  const color = theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[3];
+
   return {
     name: {
-      overflow: 'hidden', 
+      overflow: 'hidden',
       whiteSpace: 'nowrap',
+      color,
     },
     label: {
-      color: theme.colorScheme === 'dark'
-        ? theme.colors.gray[2]
-        : theme.colors.gray[3],
+      color,
     },
   };
 });
