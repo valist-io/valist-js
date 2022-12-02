f90d6a97e2f190c665dbc889840f90ef9cd5aab0
jiyuu-jin <zach@valist.io>
2022-12-02 13:41:15 -0500

    Revert previous Item change.

--- packages/valist-ui/src/components/Item/Item.styles.ts
+++ packages/valist-ui/src/components/Item/Item.styles.ts
@@ -1,16 +1,15 @@
 import { createStyles } from "@mantine/core";
 
 export default createStyles((theme, _params, _getRef) => {
-  const color = theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[3];
-
   return {
     name: {
       overflow: 'hidden',
       whiteSpace: 'nowrap',
-      color,
     },
     label: {
-      color,
+      color: theme.colorScheme === 'dark'
+        ? theme.colors.gray[2]
+        : theme.colors.gray[3],
     },
   };
 });
