c62e338570e5509fbe20cc4c862a0bf90a309357
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-12-12 10:39:26 -0800

    account select fixes

--- packages/valist-web/pages/-/activity.tsx
+++ packages/valist-web/pages/-/activity.tsx
@@ -4,28 +4,16 @@ import { useAccount } from 'wagmi';
 import useSWRImmutable from 'swr/immutable';
 import { Group } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
+import { List, Card } from '@valist/ui';
 import { Layout } from '@/components/Layout';
 import { Activity } from '@/components/Activity';
-import { Metadata } from '@/components/Metadata';
+import { AccountSelect } from '@/components/AccountSelect';
 import { useDashboard } from '@/utils/dashboard';
-
-import {
-  AccountSelect,
-  List,
-  Card,
-} from '@valist/ui';
 
 const ActivityPage: NextPage = () => {
   const { address } = useAccount();
+  const [accountName, setAccountName] = useState('');
 
-  const [accountNames, setAccountNames] = useLocalStorage<Record<string, string>>({
-    key: 'accountNames',
-    defaultValue: {},
-  });
-
-  const setAccountName = (name: string) => setAccountNames(current => ({ ...current, [`${address}`]: name }));
-
-  const accountName = address ? accountNames[address] : '';
   const { accounts, logs } = useDashboard(accountName);
   const account: any = accounts.find((a: any) => a.name === accountName);
   const { data: accountMeta } = useSWRImmutable(account?.metaURI);
@@ -33,22 +21,10 @@ const ActivityPage: NextPage = () => {
   return (
     <Layout padding={0}>
       <Group mt={40} pl={40} position="apart">
-        <AccountSelect
-          name={accountName || 'All Accounts'}
+        <AccountSelect 
           value={accountName}
-          image={accountMeta?.image}
-          href="/-/create/account"
           onChange={setAccountName}
-        >
-          <AccountSelect.Option value="" name="All Accounts" />
-          {accounts.map((acc: any, index: number) => 
-            <Metadata key={index} url={acc.metaURI}>
-              {(data: any) => (
-                <AccountSelect.Option value={acc.name} name={acc.name} image={data?.image} />
-              )}
-            </Metadata>,
-          )}
-        </AccountSelect>
+        />
       </Group>
       <div style={{ padding: 40 }}>
         <Card>
