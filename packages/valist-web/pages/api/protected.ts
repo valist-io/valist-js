0624e8baa50489be94438092e793b2bbd05a2fb2
Alec M. Wantoch <alec@wantoch.com>
2023-01-24 12:32:08 -0800

    getToken alt authenticated api route

--- packages/valist-web/pages/api/protected.ts
+++ packages/valist-web/pages/api/protected.ts
@@ -1,14 +1,28 @@
-import { getSession } from "next-auth/react";
+// import { getSession } from "next-auth/react";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   const session = await getSession({ req });
 
-  if (session) {
-    res.send(session);
+//   if (session) {
+//     res.send(session);
+//   } else {
+//     res.send({
+//       error: "Not authenticated.",
+//     });
+//   }
+// };
+
+// alternative way to do this by fetching jwt directly, but this is the straight up jwt not parsed into a session object so 'token.sub' is the address
+import { getToken } from "next-auth/jwt";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const token = await getToken({ req });
+  if (token) {
+    const address = token.sub;
+    res.send({ address });
   } else {
-    res.send({
-      error: "Not authenticated.",
-    });
+    res.status(401);
   }
+  res.end();
 };
